814bb3cea12a6e7b899bf6fa9c9910aa22996314
dianababayeva <babayeva.diana2002@gmail.com>
2023-08-23 11:16:18 +0400

    lesson 2 added

---
+++ Lesson-2/Program.cs
@@ -0,0 +1,118 @@
+﻿
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            //# region Ticket Price 
+            //            Console.WriteLine("please enter your age");
+            //            int age = Convert.ToInt32(Console.ReadLine());
+
+            //            Console.WriteLine("please enter day of week");
+            //            string dayOfWeek = Console.ReadLine();
+            //            int ticketPrice= Convert.ToInt32(Console.ReadLine());
+
+
+            //            if (dayOfWeek == "wednesday");
+            //            {
+            //                if(age>0 && age<=12)
+            //                {
+            //                    ticketPrice = 5 - 2;
+            //                }
+
+            //                else if(age>=13 && age<=64)
+            //                {
+            //                    ticketPrice = 10 - 2;
+
+            //                }
+            //                else if(age>=65)
+            //                {
+            //                    ticketPrice = 7 - 2;
+            //                }
+
+            //            else
+            //                if (age > 0 && age <= 12)
+            //                {
+            //                    ticketPrice = 5;
+            //                }
+
+            //                else if (age >= 13 && age <= 64)
+            //                {
+            //                    ticketPrice = 10;
+
+            //                }
+            //                else if (age >= 65)
+            //                {
+            //                    ticketPrice = 7;
+            //                }
+
+
+            //                Console.WriteLine(result);
+
+
+            //            } Console.WriteLine("your  ticket costs" + ticketPrice);
+
+            //            #endregion
+
+
+            ////#region score 
+            ////{
+            ////    Console.WriteLine("please enter your score");
+            ////    int num = Convert.ToInt32(Console.ReadLine());
+
+            ////    if (num > 89 && num <= 100)
+            ////    {
+            ////        Console.WriteLine("your grade is A");
+
+            ////    }
+
+            ////    else if (num > 79 || num == 89)
+            ////    {
+            ////        Console.WriteLine("your grade is B");
+
+            ////    }
+            ////    else if (num > 69 || num == 79)
+            ////    {
+            ////        Console.WriteLine("your grade is C");
+
+            ////    }
+            ////    else if (num > 59 || num == 69)
+            ////    {
+            ////        Console.WriteLine("your grade is D");
+
+            ////    }
+            ////    else if (num >= 0 || num == 59)
+            ////    {
+            ////        Console.WriteLine("your grade is E");
+
+            ////    }
+
+            ////    else
+            ////    {
+
+            ////        Console.WriteLine("wrong information");
+            ////    }
+
+            ////}
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
